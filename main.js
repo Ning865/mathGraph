df241da50639e910abea4925d8e8029c3826683a
AK47are <3318517070@qq.com>
2025-12-18 12:54:56 +0800

    style: unify code style

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 const { app, BrowserWindow } = require('electron')
-const path = require('path');
 
-function createWindow(){
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        autoHideMenuBar: true, // 自动隐藏菜单栏，按Alt键可临时显示
-        webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false// 需要设置为 false
-      }
-    })
-    win.loadFile('pages/index.html')
-    
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true, // 自动隐藏菜单栏，按 Alt 临时显示
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+  win.loadFile('pages/index.html')
 }
 
-app.on('ready', () => {
-  createWindow()
-})
+app.on('ready', () => createWindow())
